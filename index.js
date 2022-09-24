680216338015ad6855463d3df02568c89ba48801
sungfeichi <1092689732@qq.com>
2022-09-24 16:38:48 +0800

    move defered call to finally block, add tests

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-function isObj(value){
+function isObj(value) {
 	return value !== null && typeof value === "object"
 }
-function isAsync(fn){
-	return Object.prototype.toString.call (fn)=== "[object AsyncFunction]"
+function isAsync(fn) {
+	return Object.prototype.toString.call(fn) === "[object AsyncFunction]"
 }
-export function godefer(go){
-	let stack=[]
-	function defer(call){
+export function godefer(go) {
+	let stack = []
+	function defer(call) {
 		stack.push(call)
 	}
-	let blockfn=go(defer)
+	let blockfn = go(defer)
 	let asy = isAsync(blockfn)
-	if(!asy){
-		return (...args)=>{
-			let res = blockfn(...args)
-			let named = isObj(res)
-			let that = named ? res : {} 
-			for(let fc of stack.reverse()){				
-				fc.bind(that)()
+	if (!asy) {
+		return (...args) => {
+			try {
+				var res = blockfn(...args)
+			}
+			finally {
+				let named = isObj(res)
+				let that = named ? res : {}
+				for (let fc of stack.reverse()) {
+					fc.bind(that)()
+				}
 			}
 			return res
 		}
 	}
-	else{
-		return async (...args)=>{
-			let res = await blockfn(...args)
-			let named = isObj(res)
-			let that = named ? res : {} 
-			for(let fc of stack.reverse()){				
-				fc.bind(that)()
+	else {
+		return async (...args) => {
+			try {
+				var res = await blockfn(...args)
+			}
+			finally {
+				let named = isObj(res)
+				let that = named ? res : {}
+				for (let fc of stack.reverse()) {
+					fc.bind(that)()
+				}
 			}
 			return res
-		}		
+		}
 	}
 }
--- test.js
+++ test.js
@@ -1,53 +1,69 @@
-import {godefer} from './index.js'
+import { godefer } from "./index.js";
 
-let res = godefer(defer=>(name)=>{
-	console.log("begin")
-	console.log(name)
-	defer(function(){
-		//console.log(this)
-		this.a+=100
-	})
-	defer(()=>100)
-	for(let i=1;i<10;i++){
-		defer(()=>console.log(i))
+//basic usage
+let t1 = godefer((defer) => (name) => {
+	console.log("begin");
+	console.log(name);
+	for (let i = 1; i < 10; i++) {
+		defer(() => console.log(i));
 	}
+	console.log("end");
+	return 10;
+})("hello");
+console.log("t1 " + (t1 === 10 ? "success" : "fail"));
 
-	console.log("end")
-	return 10
-})('hello')
-console.log('res= ',res)
+//capture named return value
+let t2 = godefer((defer) => (name) => {
+	console.log("begin");
+	defer(function () {
+		this.a += 100;
+	});
+	console.log("end");
+	return { a: 1 };
+})("hello");
+console.log("t2 " + (t2.a === 101 ? "success" : "fail"));
 
-let res1 = godefer(defer=>(name)=>{
-	console.log("begin")
-	console.log(name)
-	defer(function(){
-		//console.log(this)
-		this.a+=100
-	})
-	defer(()=>100)
-	for(let i=1;i<10;i++){
-		defer(()=>console.log(i))
-	}
+//use try-catch
+let t3;
+try {
+	t3 = godefer((defer) => (name) => {
+		console.log("begin");
+		defer(function () {
+			console.log("defer");
+			throw new Error("defer error");
+		});
+		throw new Error("main error");
+		console.log("end");
+		return 10;
+	})("hello");
+} catch (e) {
+	console.log("t3 capture error", e.message);
+}
 
-	console.log("end")
-	return {a:1}
-})('hello')
-console.log('res1= ',res1)
+//await named return value
+let t4 = godefer((defer) => async (name) => {
+	console.log("begin");
+	defer(function () {
+		this.a += 100;
+		console.log("defer");
+	});
+	await new Promise((r) => setTimeout(r, 1000));
+	console.log("end");
+	return { a: 1 };
+})("hello");
+t4.then((v) => console.log("t4 " + (v.a === 101 ? "success" : "fail")));
 
-let res2 = godefer(defer=>async (name)=>{
-	console.log("begin")
-	console.log(name)
-	defer(function(){
-		//console.log(this)
-		//this.a+=100
-		this.a+=1000
-	})
-	defer(()=>100)
-	for(let i=1;i<10;i++){
-		defer(()=>console.log(i))
-	}
-	await new Promise(r => setTimeout(r, 1000));
-	console.log("end")
-	return {a:1}
-})('hello')
-res2.then(()=>console.log('res2= ',res2))
+//errors in async func
+let t5 = godefer((defer) => async (name) => {
+	console.log("begin");
+	defer(function () {
+		this.a += 100;
+		console.log("defer");
+		throw new Error("defer error");
+	});
+	await new Promise((r) => setTimeout(r, 1000));
+	throw new Error("main error");
+	console.log("end");
+	return { a: 1 };
+})("hello");
+t5.catch((e) => console.log("t5 capture error", e.message));
